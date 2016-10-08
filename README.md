52117f3076ef771f3082854052668e1963678fe8
Ralf S. Engelschall <rse@engelschall.com>
2016-10-08 20:38:50 +0200

    initially place under version control

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# installed-packages
-Determine names of all installed packages
+
+installed-packages
+==================
+
+Determine names of all installed packages.
+
+<p/>
+<img src="https://nodei.co/npm/installed-packages.png?downloads=true&stars=true" alt=""/>
+
+<p/>
+<img src="https://david-dm.org/rse/installed-packages.png" alt=""/>
+
+About
+-----
+
+This is a small JavaScript library for use in Node.js environments,
+providing the possibility to determine all installed packages.
+
+The packages to be considered are: all built-in modules, all packages in the
+global NPM directory, all packages in all `node_modules` directories of the
+current and all parent directories and all packages in the directories
+of environment variable `NODE_PATH`.
+
+The motivation for this module is the following scenario: a tool
+`foo` wants to allow plugins to extend itself. The plugins, by
+convention, have to be named `foo-plugin-xxx`. On startup, `foo` uses
+`installed-packages` to determine all available packages, filters the
+list via pattern `foo-plugin-*` and then `require`s all found plugin
+packages and give them a chance to hook into the processing of `foo`.
+
+Example
+-------
+
+```
+const installedPackages = require("installed-packages")
+
+installedPackages().then((packages) => {
+    console.log(packages)
+}, (err) => {
+    console.log(`ERROR: ${err}`)
+})
+```
+
+Installation
+------------
+
+```shell
+$ npm install installed-packages
+```
+
+License
+-------
+
+Copyright (c) 2016 Ralf S. Engelschall (http://engelschall.com/)
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ installed-packages.js
@@ -0,0 +1,128 @@
+/*
+**  installed-packages -- Determine names of all installed packages
+**  Copyright (c) 2016 Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*  built-in requirements  */
+var path           = require("path")
+var npm            = require("npm")
+
+/*  external requirements  */
+var fs             = require("fs-promise")
+var co             = require("co")
+var builtinModules = require("builtin-modules")
+
+/*  the single API method  */
+const installedPackages = co.wrap(function * (cache) {
+    let packages = []
+
+    /*  initialize optional cache  */
+    if (typeof cache !== "object")
+        cache = {}
+
+    /*  determine all packages in a particular directory  */
+    const findAllPackagesInDir = co.wrap(function * (dir) {
+        let packages = []
+
+        /*  iterate over all files in directory  */
+        let items = yield fs.readdir(dir)
+        for (let i = 0; i < items.length; i++) {
+
+            /*  in case the file is a directory  */
+            let stat = yield fs.stat(path.join(dir, items[i]))
+            if (stat.isDirectory()) {
+
+                /*  take the directory if a file "package.json" is also present in it  */
+                stat = yield fs.stat(path.join(dir, items[i], "package.json"))
+                    .catch(() => null)
+                if (stat !== null && stat.isFile())
+                    packages.push(items[i])
+            }
+        }
+        return packages
+    })
+
+    /*  determine all built-in modules  */
+    packages = packages.concat(builtinModules)
+
+    /*  determine global NPM package directory  */
+    let globalDir = yield new Promise((resolve, reject) => {
+        npm.load((err, npm) => {
+            if (err) reject(err)
+            else     resolve(npm.globalDir)
+        })
+    })
+
+    /*  determine all globally installed NPM packages  */
+    let p = yield findAllPackagesInDir(globalDir)
+    packages = packages.concat(p)
+
+    /*  iterate over all paths up to the root directory...  */
+    let dirPrev
+    let dirNow = process.cwd()
+    while (dirNow !== dirPrev) {
+
+        /*  check whether a "node_modules" directory exists inside it  */
+        let dir = path.join(dirNow, "node_modules")
+        let s = yield fs.stat(dir)
+            .catch(() => null)
+        if (s !== null && s.isDirectory()) {
+
+            /*  determine all locally installed NPM packages  */
+            let p = yield findAllPackagesInDir(dir)
+            packages = packages.concat(p)
+        }
+
+        /*  walk to the parent directpry  */
+        dirPrev = dirNow
+        dirNow = path.resolve(dirNow, "..")
+    }
+
+    /*  iterate over all custom NPM module paths  */
+    let nodePath = process.env.NODE_PATH
+    if (typeof nodePath === "string" && nodePath !== "") {
+        nodePath = nodePath.split(path.delimiter)
+        for (let i = 0; i < nodePath.length; i++) {
+
+            /*  check whether item is a directory  */
+            let s = yield fs.stat(nodePath[i])
+                .catch(() => null)
+            if (s !== null && s.isDirectory()) {
+
+                /*  determine all locally installed NPM packages  */
+                let p = yield findAllPackagesInDir(nodePath[i])
+                packages = packages.concat(p)
+            }
+        }
+    }
+
+    /*  de-duplicate packages  */
+    packages = packages.filter((elem, pos) => {
+        return packages.indexOf(elem) === pos
+    })
+
+    return packages
+})
+
+/*  export API method  */
+module.exports = installedPackages
+
